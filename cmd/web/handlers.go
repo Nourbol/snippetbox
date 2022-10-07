db00f628a822887c96cd807a42ca746ed0ef8bb4
Aldiyar Rakhimzhanov <aldiyar.rakhimzhanov@intexsoft.kz>
2022-10-07 11:53:50 +0600

    Structure of the project was changed. The first HTML template was added.
    The project uses the popular file-structure in GO. We broke down into three main directories: cmd, internal and ui. In cmd we separate handler functions into handlers.go. Now in main.go, there is only execution of the application.
    
    Additionally, the project uses templates now. For example, we can view home.tmpl requesting with "/home" url.

---
+++ cmd/web/handlers.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
