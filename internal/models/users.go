7c47b70053d5ad69b240259ed4f1b72bd4e8de60
Nurbol Bektibay <nurbol.bektibay@intexsoft.kz>
2022-12-03 21:52:50 +0600

    User login was made. Errors that are not related to the specific field were added into Validator

--- internal/models/users.go
+++ internal/models/users.go
@@ -43,7 +43,26 @@ func (m *UserModel) Insert(name, email, password string) error {
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPassword []byte
+
+	stmt := `SELECT id, hashed_password FROM users WHERE email = ?`
+
+	if err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		}
+		return 0, err
+	}
+
+	if err := bcrypt.CompareHashAndPassword(hashedPassword, []byte(password)); err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		}
+		return 0, err
+	}
+
+	return id, nil
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
